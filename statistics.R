718e44b8c96532587d048ef9be9f6bf40b7e32ef
kanishkaryaa <77195854+kanishkaryaa@users.noreply.github.com>
2023-02-17 15:46:26 +0530

    Add files via upload

---
+++ statistics.R
@@ -0,0 +1,13 @@
+
+
+min_median_range <- function(data){
+
+  data = c(23, 4, 56, 21, 34, 56, 73)
+
+  # get the minimum value
+  print(min(data))
+  print(median(data))
+  print(range(data))
+
+}
+min_median_range(data)
